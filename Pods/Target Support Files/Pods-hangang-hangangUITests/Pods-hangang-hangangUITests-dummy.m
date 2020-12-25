f775f9e797c2bd9e48fde153e6f14e3c97efeabe
Bluebada <wxg1297@gmail.com>
2020-12-25 17:24:02 +0900

    멀티플랫폼용 swiftui 프로젝트 생성

---
+++ Pods/Target Support Files/Pods-hangang-hangangUITests/Pods-hangang-hangangUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_hangang_hangangUITests : NSObject
+@end
+@implementation PodsDummy_Pods_hangang_hangangUITests
+@end
---
+++ Pods/Target Support Files/Pods-hangang/Pods-hangang-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_hangang : NSObject
+@end
+@implementation PodsDummy_Pods_hangang
+@end
